12d7d05cea95a5eb0d2ef7126c2320be61227da7
n_joid <72697791+nakisara01@users.noreply.github.com>
2026-02-06 01:04:51 +0900

    feat: add initialView layout & image

--- gimbapsori.swiftpm/Presentation/View/initial/InitialView.swift
+++ gimbapsori.swiftpm/Presentation/View/initial/InitialView.swift
@@ -5,3 +5,30 @@
 //  Created by 나현흠 on 2/6/26.
 //
 
+import SwiftUI
+
+struct InitialView: View {
+    var backgroundColor: Color = .brown
+    
+    var body: some View {
+        ZStack{
+            Image("GimPopSori_Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            Color.white.opacity(0.6)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("GimPopSori_Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 200)
+            }
+        }
+    }
+}
+
+#Preview {
+    InitialView()
+}
